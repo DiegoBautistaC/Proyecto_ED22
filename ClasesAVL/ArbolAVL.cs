5f1560df914b224b0b3617b61f294ed5f84c5040
DiegoBautista <da10bautista@gmail.com>
2022-04-08 21:27:58 -0600

    Creación de las clases ArbolAVL y NodoAVL

---
+++ ClasesAVL/ArbolAVL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesAVL
+{
+    public class ArbolAVL <T>
+    {
+        public NodoAVL<T> Raiz;
+
+        public delegate int Delegado(T valor1, T valor2);
+
+        public Delegado Comparador;
+
+        public ArbolAVL(Delegado unComparador)
+        {
+            this.Comparador = unComparador;
+        }
+    }
+}
