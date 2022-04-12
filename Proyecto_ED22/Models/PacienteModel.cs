696b11a7cea32ddf769321583ad85ed390a9d9f3
Dougl4Salazar <92768320+Dougl4Salazar@users.noreply.github.com>
2022-04-12 11:07:30 -0600

    Modelo paciente y sus atributos

---
+++ Proyecto_ED22/Models/PacienteModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto_ED22.Models
+{
+    public class PacienteModel
+    {
+    }
+}
