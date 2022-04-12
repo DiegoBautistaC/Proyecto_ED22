1f6b8c5a25bc52f657c84600cc7f32fa90d96fe8
DiegoBautista <da10bautista@gmail.com>
2022-04-12 11:44:36 -0600

    Subo correctamente (invalidar commit)

--- Proyecto_ED22/Helpers/Data.cs
+++ Proyecto_ED22/Helpers/Data.cs
@@ -2,10 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ClasesAVL;
+using Proyecto_ED22.Models;
 
 namespace Proyecto_ED22.Helpers
 {
     public class Data
     {
+        private static Data _instance = null;
+
+        public static Data Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Data();
+                }
+                return _instance;
+            }
+        }
+
+        public ArbolAVL<PacienteModel> ArbolAVL_NombresPacientes = new ArbolAVL<PacienteModel>((PacienteModel paciente1, PacienteModel paciente2) => paciente1.Nombre.CompareTo(paciente2.Nombre), (string unNombre, PacienteModel unPaciente) => unNombre.CompareTo(unPaciente.Nombre));
+        
+        public ArbolAVL<PacienteModel> ArbolAVL_DPIPacientes = new ArbolAVL<PacienteModel>((PacienteModel paciente1, PacienteModel paciente2) => paciente1.DPI - paciente2.DPI, (string unDPI, PacienteModel unPaciente) => Convert.ToInt32(unDPI) - unPaciente.DPI);
     }
 }
