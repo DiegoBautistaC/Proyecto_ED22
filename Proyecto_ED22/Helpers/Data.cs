2dc1ab30415c85631e09212b72b8829ed1e2369b
Dougl4Salazar <92768320+Dougl4Salazar@users.noreply.github.com>
2022-04-15 17:19:40 -0600

    Correcciones

--- Proyecto_ED22/Helpers/Data.cs
+++ Proyecto_ED22/Helpers/Data.cs
@@ -25,6 +25,6 @@ namespace Proyecto_ED22.Helpers
 
         public ArbolAVL<PacienteModel> ArbolAVL_NombresPacientes = new ArbolAVL<PacienteModel>((PacienteModel paciente1, PacienteModel paciente2) => paciente1.Nombre.CompareTo(paciente2.Nombre), (string unNombre, PacienteModel unPaciente) => unNombre.CompareTo(unPaciente.Nombre));
         
-        public ArbolAVL<PacienteModel> ArbolAVL_DPIPacientes = new ArbolAVL<PacienteModel>((PacienteModel paciente1, PacienteModel paciente2) => Convert.ToInt32(paciente1.DPI - paciente2.DPI), (string unDPI, PacienteModel unPaciente) => Convert.ToInt32(Convert.ToInt64(unDPI) - unPaciente.DPI));
+        public ArbolAVL<PacienteModel> ArbolAVL_DPIPacientes = new ArbolAVL<PacienteModel>((PacienteModel paciente1, PacienteModel paciente2) => paciente1.DPI.CompareTo(paciente2.DPI) , (string unDPI, PacienteModel unPaciente) => unDPI.CompareTo(unPaciente.DPI));
     }
 }
